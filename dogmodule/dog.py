d65bf6b753fe852e9690b17995f3bc3402b5e914
donkere-vader <donkere.v@gmail.com>
2021-08-26 17:53:38 +0200

    updated Dog module

--- dogmodule/dog.py
+++ dogmodule/dog.py
@@ -1,34 +1,57 @@
 import os
-import json
+import yaml
+
 
 
 class Dog:
-    def __init__(self, file="dog.json", folder="dog"):
-        files = os.listdir()
+    def __init__(self, command, list, file, verbose):
+        self.file = file
+        self.verbose = verbose
+        self.list = list
 
-        self.file, self.folder = file, folder
+        if self.file not in os.listdir():
+            print(f"Couldn't find '{self.file}' in current working directory")
+            exit(1)
 
-        # check if dog is instanciated
-        if self.file in files:
-            file_loc = self.file
-        elif self.folder in files and os.path.isdir(self.folder) and self.file in os.listdir(self.folder):
-            file_loc = f"{self.folder}/{self.file}"
-        else:
-            print(f"No {self.file} or {self.folder}/{self.file} file found.")
-            exit()
+        self.info: dict = {}
+        with open(self.file, 'r') as f:
+            self.info: dict = yaml.load(f, Loader=yaml.FullLoader)
 
-        self.commands = self.load_commands(file_loc)['commands']
+        if 'commands' not in self.info:
+            print(f"Please supply commands in the '{self.file}' dog file")
+            exit(1)
 
-    def load_commands(self, json_file):
-        return json.load(open(json_file, 'r'))
+        if self.list:
+            self.show_list()
+            exit(0)
 
-    def command(self, command):
-        try:
-            command = self.commands[command]
-        except KeyError:
-            raise Exception(f"No command '{command}' found in {self.file} or {self.folder}/{self.file}")
+        self.run_command(command)
+    
+    def show_list(self):
+        print("List of available commands:\n")
+        for cmd in self.info['commands']:
+            print(f"  {cmd}")
+            if 'description' in self.info['commands'][cmd]:
+                print(f"    {self.info['commands'][cmd]['description']}")
+        
+        print()
+        print("usage: ``dog COMMAND``")
 
-        if type(command) == dict:
-            command = f"{self.folder}/{command['file']}"
+    def run_command(self, command):
+        if self.verbose: print(f"Running DOG command: {command}")
 
-        os.system(command)
+        if command not in self.info['commands']:
+            print(f"Command '{command}' not found in dog file: '{self.file}'")
+            print("For a list of commands run ``dog --list`` ")
+            exit(1)
+
+        code = [line.strip() for line in str(self.info['commands'][command]['code']).split(";")]
+
+        while "" in code:
+            code.remove("")
+
+        for line in code:
+            if self.verbose:
+                print()
+                print("$ " + line.replace('\n', '\\n'))
+            os.system(line)
